2693acb96b6c2bd04ce6657daf235720814eb3b1
S. M. Redwan <smredwan.info@gmail.com>
2025-03-27 19:49:22 +0600

    add database connection and implement user model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,5 +12,6 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
