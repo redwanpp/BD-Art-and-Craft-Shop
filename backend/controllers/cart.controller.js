6c5a92aea7c84cc6e381c717829676eab87d6a97
S. M. Redwan <smredwan.info@gmail.com>
2025-03-28 12:14:29 +0600

    complete cart backend

---
+++ backend/controllers/cart.controller.js
@@ -0,0 +1,79 @@
+import Product from "../models/product.model.js";
+export const getCartProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ _id: { $in: req.user.cartItems } });
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+
+    res.json(cartItems);
+  } catch (error) {
+    console.log("Error in getCartProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const addToCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+
+    const exitingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (exitingItem) {
+      exitingItem.quantity += 1;
+    } else {
+      user.cartItems.push({ id: productId, quantity: 1 });
+    }
+
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    console.log("Error in addToCart:", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+    }
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const exitingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (exitingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        res.json(user.cartItems);
+      }
+      exitingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    } else {
+      res.status(404).json({ message: "Item not found in cart" });
+    }
+  } catch (error) {
+    console.log("Error in updateQuantity:", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
