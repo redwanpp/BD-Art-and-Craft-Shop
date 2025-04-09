d1943b576a10a7156c94a9c5a553248b72699b03
NadimMahmudRishan <rishan35-952@diu.edu.bd>
2025-04-09 07:27:41 -0700

    Cart page added but not complete.

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,21 @@ import { useUserStore } from "./stores/useUserStore";
 import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import { AdminPage } from "./pages/AdminPage";
+import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  const { getCartItems } = useCartStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    getCartItems();
+  }, [getCartItems]);
 
   if (checkingAuth) return <LoadingSpinner />;
 
@@ -49,6 +57,11 @@ function App() {
           />
 
           <Route path="/category/:category" element={<CategoryPage />} />
+
+          <Route
+            path="/cart"
+            element={user ? <CartPage /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
       <Toaster />
