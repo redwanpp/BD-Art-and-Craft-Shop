10f7e2543ba52d6982ff39d3503db9ddba23e2a9
NadimMahmudRishan <rishan35-952@diu.edu.bd>
2025-04-10 10:37:39 -0700

    PurchaseCanclePage created but not complete.

---
+++ src/pages/PurchaseCancelPage.jsx
@@ -0,0 +1,12 @@
+import { XCircle, ArrowLeft } from "lucide-react";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+const PurchaseCancelPage = () => {
+	return (
+		<div>Order cancalled</div>
+	);
+};
+
+export default PurchaseCancelPage;
