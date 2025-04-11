2da7049c6a017927f806886842eb879f62d73950
NadimMahmudRishan <rishan35-952@diu.edu.bd>
2025-04-10 23:04:09 -0700

    Data featch problem solve into in Featured Product

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
-import CategoryItem from "../components/CategoryItem";
-import { useProductStore } from "../stores/useProductStore";
-import FeaturedProducts from "../components/FeaturedProducts";
+import React, { useEffect } from 'react'
+import CategoryItem from '../components/CategoryItem';
+import { useProductStore } from '../stores/useProductStore';
+import FeaturedProducts from '../components/FeaturedProducts';
 
 const categories = [
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
-  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
-  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
-  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
-  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
-  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+  {href: "/arts", name: "Arts", imageUrl: "/images/arts/img1.jpeg"},
+  {href: "/bamboproducts", name: "Bamboo Products", imageUrl: "/images/bambooProducts/img1.jpeg"},
+  {href: "/nakshikhantha", name: "Nakshi Kantha", imageUrl: "/images/nakshiKantha/img1.jpg"},
+  {href: "tatkapoor", name: "Tat Kapoor", imageUrl: "/images/tatKapoor/img1.jpg"},
+  {href: "/teracotta", name: "Teracotta", imageUrl: "/images/teracotta/img1.jpg"},
+  {href: "/woodProducts", name: "Wood Products", imageUrl: "/images/woodProducts/img1.webp"},
 ];
 
 const HomePage = () => {
+
   const { fetchFeaturedProducts, products, isLoading } = useProductStore();
 
   useEffect(() => {
@@ -22,12 +22,12 @@ const HomePage = () => {
 
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <h1 className="text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4">
+      <div className="relative z-10 max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <h1 className="text-center text-4xl sm:text-5xl font-bold text-cyan-400 mb-4">
           Explore Our Categories
         </h1>
         <p className="text-center text-xl text-gray-300 mb-12">
-          Discover the latest trends in eco-friendly fashion
+          Discover the latest trends in eco-friendly products
         </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -35,12 +35,12 @@ const HomePage = () => {
             <CategoryItem category={category} key={category.name} />
           ))}
         </div>
-
         {!isLoading && products.length > 0 && (
           <FeaturedProducts featuredProducts={products} />
         )}
       </div>
     </div>
   );
-};
-export default HomePage;
+}
+
+export default HomePage
